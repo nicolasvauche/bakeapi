84e166cdee9004adcb65604ebcd0507258b6bc4e
Nicolas vauché <hello@nicolasvauche.net>
2024-03-13 11:47:10 +0100

    Without the suite

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -20,19 +20,6 @@ module.exports = db => {
             .status(500)
             .json({ error: error.message || 'Internal server error' })
         })
-    },
-    getProducts: (req, res) => {
-      const { _id } = req.userInfos
-      Product.findAllByUserId(_id)
-        .then(result => {
-          res.status(200).json(result)
-        })
-        .catch(error => {
-          console.error('Internal server error:', error)
-          res
-            .status(500)
-            .json({ error: error.message || 'Internal server error' })
-        })
-    },
+    }
   }
 }
