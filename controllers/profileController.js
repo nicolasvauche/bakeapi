fa3d9c7a153be58d94b552e1aeecdedaaf904b3d
Nicolas vauché <hello@nicolasvauche.net>
2024-03-13 12:59:29 +0100

    Admin: User CRUD

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -7,10 +7,6 @@ module.exports = db => {
       const { _id } = req.userInfos
       User.findById(_id)
         .then(result => {
-          if (!result) {
-            return res.status(404).json({ error: 'User not found' })
-          }
-
           const { password, ...user } = result
           res.status(200).json(user)
         })
@@ -33,6 +29,6 @@ module.exports = db => {
             .status(500)
             .json({ error: error.message || 'Internal server error' })
         })
-    },
+    }
   }
 }
